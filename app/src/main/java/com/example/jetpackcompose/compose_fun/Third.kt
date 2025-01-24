54d21d0685f9d6280a579720911e43919531e1c7
s.sika <y.sika@abtollc.com>
2025-01-24 19:40:15 +0100

    minor changes

--- app/src/main/java/com/example/jetpackcompose/compose_fun/Third.kt
+++ app/src/main/java/com/example/jetpackcompose/compose_fun/Third.kt
@@ -11,10 +11,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -33,6 +36,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.jetpackcompose.R
+import com.example.jetpackcompose.support.showToast
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -55,7 +59,18 @@ fun ThirdFragmentScreen(
                     Icon(Icons.Filled.ArrowBack, contentDescription = null)
                 }
             })
-        }
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                modifier = Modifier.padding(bottom = 64.dp),
+                onClick = {
+                    showToast(context, "FloatingActionButton")
+                }
+            ) {
+                Icon(Icons.Filled.Add, contentDescription = "FAB Icon")
+            }
+        },
+        floatingActionButtonPosition = FabPosition.End
     ) {
         Modifier.padding(it)
         Box(
